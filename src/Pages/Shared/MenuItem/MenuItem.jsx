d16f8df0bb2970f31cb856c1e828b5a968045e6a
Amran Hossain <amran.h.akash@gmail.com>
2025-01-23 21:53:22 +0600

    components

--- src/Pages/Shared/MenuItem/MenuItem.jsx
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const MenuItem = ({ item }) => {
+    const { name, image, price, recipe } = item;
     return (
-        <div>
+        <div className='flex space-x-2'>
+            <img style={{ borderRadius: '0 200px 200px 200px' }} className="w-[104px]" src={image} alt="" />
+            <div>
+                <h3>{name}---------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className='text-yellow-500'>{price}</p>
 
         </div>
     );
