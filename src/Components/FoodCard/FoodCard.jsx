63f6ba837996eb19622c053768cb41c2c8b46e06
Amran Hossain <amran.h.akash@gmail.com>
2025-01-24 21:11:43 +0600

    server created

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -11,11 +11,12 @@ const FoodCard = ({ item }) => {
             </figure>
             <p className='bg-slate-900 text-white absolute right-0 mr-5 mt-5 px-4
 rounded-lg'>${price}</p>
-            <div className="card-body text-center">
+            <div className="card-body  flex flex-col items-center">
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Add to Cart</button>
+                    <button className='btn btn-outline bg-slate-100 text-yellow-600 border-0 border-b-4 mt-4'>Add to Cart</button>
+
                 </div>
             </div>
         </div>
--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -4,13 +4,12 @@ import Cover from '../../Shared/Cover/Cover';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import useMenu from '../../../hooks/useMenu';
-import FoodCard from '../../../Components/FoodCard/FoodCard';
 import OrderTab from '../OrderTab/OrderTab';
 import { useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 
 const Order = () => {
-    const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks']
+    const categories = ['salad', 'pizza', 'soup', 'desserts', 'drinks']
     const { category } = useParams();
     const initialIndex = categories.indexOf(category);
     const [tabIndex, setTabIndex] = useState(initialIndex)
@@ -33,7 +32,7 @@ const Order = () => {
                     <Tab>Salad</Tab>
                     <Tab>Pizza</Tab>
                     <Tab>Soup</Tab>
-                    <Tab>Dessert</Tab>
+                    <Tab>Desserts</Tab>
                     <Tab>Drinks</Tab>
                 </TabList>
                 <TabPanel>
