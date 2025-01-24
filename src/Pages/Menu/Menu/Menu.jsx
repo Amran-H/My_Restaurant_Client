ee70aaf2e8990afb38657b9da1a3199b0ce98d01
Amran Hossain <amran.h.akash@gmail.com>
2025-01-24 15:51:46 +0600

    menu page

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -2,16 +2,35 @@ import React from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/menu-bg.jpg';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
+import saladImg from '../../../assets/menu/salad-bg.jpg';
+import soupImg from '../../../assets/menu/soup-bg.jpg';
 import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === "dessert")
+    const soup = menu.filter(item => item.category === "soup")
+    const salad = menu.filter(item => item.category === "salad")
+    const pizza = menu.filter(item => item.category === "pizza")
+    const offered = menu.filter(item => item.category === "offered")
+
     return (
         <div>
             <Helmet>
                 <title>Our Menu</title>
             </Helmet>
             <Cover img={menuImg} title={"Our Menu"}></Cover>
-            <PopularMenu></PopularMenu>
+            <SectionTitle heading={"Today's Offer"} subHeading={"Don't miss"}></SectionTitle>
+            <MenuCategory items={offered}></MenuCategory>
+            <MenuCategory items={desserts} title={"Desserts"} img={dessertImg}></MenuCategory>
+            <MenuCategory items={pizza} title={"Pizza"} img={pizzaImg}></MenuCategory>
+            <MenuCategory items={salad} title={"Salad"} img={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={"Soup"} img={soupImg}></MenuCategory>
         </div>
     );
 };
