e48f6e869e3b77b711af56371aa9a8f8ed219725
Amran Hossain <amran.h.akash@gmail.com>
2025-01-24 21:45:45 +0600

    various css added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import './Banner.css'; // Import the custom CSS
 import img1 from "../../../assets/home/01.jpg"
 import img2 from "../../../assets/home/02.jpg"
 import img3 from "../../../assets/home/03.png"
@@ -9,7 +10,12 @@ import img6 from "../../../assets/home/06.png"
 
 const Banner = () => {
     return (
-        <Carousel>
+        <Carousel
+            showThumbs={true}
+            className="custom-carousel"
+        // showStatus={false} // Optional: hides the status text
+        // infiniteLoop={false} // Optional: enables looping
+        >
             <div>
                 <img src={img1} />
             </div>
--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -9,22 +9,23 @@ import Slide3 from "../../../assets/home/slide3.jpg"
 import Slide4 from "../../../assets/home/slide4.jpg"
 import Slide5 from "../../../assets/home/slide5.jpg"
 import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import './Category.css'; // Custom CSS file for Swiper styling
 
 const Category = () => {
     return (
-        <section>
+        <section className='mx-10'>
             <SectionTitle heading={"Order Online"}
                 subHeading={"From 11.00am to 10.00pm"}>
             </SectionTitle>
             <Swiper
                 slidesPerView={4}
                 spaceBetween={30}
-                centeredSlides={true}
+                centeredSlides={false}
                 pagination={{
                     clickable: true,
                 }}
                 modules={[Pagination]}
-                className="mySwiper mb-20"
+                className="mySwiper "
             >
                 <SwiperSlide><img src={Slide1} alt="" />
                     <h3 className='uppercase text-4xl -mt-16 text-white text-center'>Salads</h3>
