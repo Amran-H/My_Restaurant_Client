43ede306c88c2e3fc2ca51f61f39898024bfbc07
Amran Hossain <amran.h.akash@gmail.com>
2025-02-24 22:16:56 +0600

    login form added1

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { LoadCanvasTemplate, validateCaptcha, loadCaptchaEnginge } from 'react-simple-captcha';
 
 const Login = () => {
+    const captchaRef = useRef(null);
+    const [disabled, setDisabled] = useState(true);
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, [])
+
     const handleLogin = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password)
+    }
+
+    const handleValidateCaptcha = () => {
+        const captcha = captchaRef.current.value;
+        if (validateCaptcha(captcha)) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
     }
 
     return (
@@ -35,8 +51,15 @@ const Login = () => {
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <LoadCanvasTemplate />
+                            </label>
+                            <input type="text" ref={captchaRef} name='captcha' placeholder="Type the text captcha" className="input input-bordered" required />
+                            <button onClick={handleValidateCaptcha} className='btn btn-outline btn-xs mt-2'>Validate</button>
+                        </div>
                         <div className="form-control mt-6">
-                            <input className="btn btn-primary" type="submit" value='Login' />
+                            <input disabled={disabled} className="btn btn-primary" type="submit" value='Login' />
                         </div>
                     </form>
                 </div>
